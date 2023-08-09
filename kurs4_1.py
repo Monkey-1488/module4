b0129516a0c47fe083f3c3d8f26b4b1fc7ce4854
Monkey-1488 <Rikardomilos.2019@mail.ru>
2023-08-09 20:34:12 +0500

    домашка

--- kurs4_1.py
+++ kurs4_1.py
@@ -9,23 +9,37 @@ g-1
 
 """
 
-def strcounter(s):
-    for sym in set(s):
-        count = 0
-        for sub_sym in s:
-            if sym == sub_sym:
-                count += 1
-        print(f'{sym} - {count}')
+# def strcounter(s):
+#     for sym in set(s):
+#         count = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 count += 1
+#         print(f'{sym} - {count}')
 
-strcounter('assdfg')
+# strcounter('assdfg')
 
-# O(N)
-def strcounter(s):
-    sym_dict = {}
-    for sym in s:
-        sym_dict[sym] = 1 + sym_dict.get(sym, 0)
+# # O(N)
+# def strcounter(s):
+#     sym_dict = {}
+#     for sym in s:
+#         sym_dict[sym] = 1 + sym_dict.get(sym, 0)
 
-    for sym, count in sym_dict.items():
-        print(f'{sym} - {count}')
+#     for sym, count in sym_dict.items():
+#         print(f'{sym} - {count}')
 
-strcounter('zxc')
+# strcounter('zxc')
+
+
+
+DZ
+def is_palindrome(s):
+    return s == s[::-1]
+
+input_str = input("Введите строку: ").lower()
+input_str = input_str.replace(" ", "")
+
+if is_palindrome(input_str):
+    print("True")
+else:
+    print("False")
