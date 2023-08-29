bc33a900f8fa9111d6c6b9120cc382ea8573af64
Monkey-1488 <Rikardomilos.2019@mail.ru>
2023-08-29 17:42:27 +0500

    urok7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=3)
+    auction = models.BooleanField('Торг', help_text='Укажите, возможен ли торг')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+
+
+"""
+Название
+Описание
+Цена
+Дата создания
+Дата обновления
+Торг
+"""
